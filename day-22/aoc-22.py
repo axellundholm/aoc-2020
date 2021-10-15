4f44ef9e86ca8710e92d69734e13f03217385415
axellundholm <axel.lundholm@me.com>
2021-10-15 11:16:22 +0200

    day-22

--- day-22/aoc-22.py
+++ day-22/aoc-22.py
@@ -1,3 +1,5 @@
+import time
+
 def is_num(number):
 	try:
 		int(number)
@@ -22,27 +24,56 @@ def create_decks(list):
 
 	return decks
 	
-def start_round(decks):
-	
+def start_round(decks, recursion_mode = False):
+	print "ONE"
 	cards = []
+	recurse = True
+	time.sleep(0.1)
+	
 	for p in decks:
 		cards.append(p.pop(0))
+		if len(p) < cards[-1]: recurse = False
+	
+	if recurse and recursion_mode:
+		sub_decks = [decks[i][0:cards[i]] for i in range(len(decks))]
+		winner, _ = start_recursive_combat_game(sub_decks)
+		decks[winner].append(cards.pop(winner))
+		decks[winner].extend(cards)
+		return decks
 	
 	decks[cards.index(max(cards))].extend(sorted(cards, reverse=True))
-	
-	return decks	
+	return decks
 
 def start_combat_game(decks):
+	print "TWO"
+	
 	
 	while True:
+		print "THREE"
 		if min([len(x) for x in decks]) == 0: break
 		decks = start_round(decks)
+	
+	winner = [len(x) for x in decks].index(max([len(x) for x in decks]))
+	return winner, decks
+
+def start_recursive_combat_game(decks):
+	print "FOUR"
+	
+	prev_decks = set()
+	
+	while True:
+		print "FIVE"
+		if min([len(x) for x in decks]) == 0: break
+		if decks[0] in prev_decks and decks[1] in prev_decks: return 0, decks
+		decks = start_round(decks, True)
 		
-	return decks
+		decks_config = (tuple(decks[0]), tuple(decks[1]))
+		prev_decks.add(decks_config) 
 	
-def count_points(game):
+	winner = [len(x) for x in decks].index(max([len(x) for x in decks]))
+	return winner, decks
 	
-	winner = [len(x) for x in game].index(max([len(x) for x in game]))
+def count_points(winner, game):
 	
 	points = 0
 	multiplier = 1
@@ -60,10 +91,15 @@ if __name__ == "__main__":
 	list = file.readlines()
 	list = [x.strip() for x in list]
 
+	# decks = create_decks(list)
+	# winner, game = start_combat_game(decks)
+	# points = count_points(winner, game)
+	# print points
+	
 	decks = create_decks(list)
-	game = start_combat_game(decks)
-	points = count_points(game)
+	recursion_winner, recursion_game = start_recursive_combat_game(decks)
+	recursion_points = count_points(recursion_winner, recursion_game)
+	print recursion_points
 
-	print points
 	
 	
